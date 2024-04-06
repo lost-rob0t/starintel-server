9c4f1d1b9d4697f089c8ffc2f814c38d189ba9f6
nsaspy <nsaspy@airmail.cc>
2024-04-05 20:34:52 -0400

    add files

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Starintel API server that routes the data through msg queues.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux =
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkg-config
+            sbcl
+            openssl
+            rabbitmq-c
+          ];
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath([pkgs.openssl pkgs.rabbitmq-c])}
+          '';
+        };
+    };
+}
