42e7dd8dee2cadafd9cb4411d06184c051bb6bf1
N545PY <nsaspy@fedora.email>
2024-09-06 07:06:50 -0400

    remove junk

--- source/http-api.lisp
+++
@@ -1,8 +0,0 @@
-;; [[file:../source.org::*Get Documents][Get Documents:1]]
-(setf (ningle:route *app* "/document/:id" :method :get)
-      #'(lambda (params)
-
-          (let ((document-id  (cdr (assoc :id params :test #'string=))))
-
-            document-id)))
-;; Get Documents:1 ends here
