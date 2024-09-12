306c16632a970b6e31c61e551057a410b07bbf03
N545PY <nsaspy@fedora.email>
2024-09-12 19:18:27 -0400

    add more format cases

--- source/databases/couchdb.lisp
+++ source/databases/couchdb.lisp
@@ -29,16 +29,29 @@
                      (t (to-json value)))))
     json-obj))
 
+(defun camel-case-to-lisp-case (string)
+  (with-output-to-string (s)
+    (loop for char across string
+          for i from 0
+          do (cond
+               ((and (not (zerop i))
+                     (upper-case-p char))
+                (write-char #\- s)
+                (write-char (char-downcase char) s))
+               (t (write-char (char-downcase char) s))))))
 
+(defun format-key (key)
+  (if (str:starts-with? "_" key)
+      (string-downcase key)
+      (camel-case-to-lisp-case key)))
 
-
-(defun from-json (json-obj class-name)
+(defun from-json (json-obj class-name &key (format-fn #'format-key))
   (let* ((object (make-instance class-name))
          (class (class-of object)))
     (loop for slot in (sb-mop:class-slots class)
           for slot-name = (sb-mop:slot-definition-name slot)
           for slot-type = (sb-mop:slot-definition-type slot)
-          for key = (string-downcase (string slot-name))
+          for key = (funcall format-fn (string slot-name))
           for value = (jsown:val-safe json-obj key)
           when value
             do (setf (slot-value object slot-name)
@@ -48,6 +61,10 @@
                        ((eq slot-type 'integer) value)
                        (t (from-json value (eval slot-type))))))
     object))
+
+
+
+
 
 
 
