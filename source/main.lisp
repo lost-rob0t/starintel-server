c680bf6ab42db6d669a4805fb23095c6e7e45c0b
N545PY <nsaspy@fedora.email>
2024-10-20 19:26:56 -0400

    add cli args!

--- source/main.lisp
+++ source/main.lisp
@@ -16,45 +16,12 @@
     :long-name "debugger"
     :key :debugger)))
 
-;; [[file:../source.org::*Server command handler][Server command handler:1]]
-;; (defun server/handler (cmd))
-;; (let ((debugger (clingon:getopt cmd :debugger))
-;;       (init-file (clingon:getopt cmd :init-value)))
-;;   (when debugger
-;;     (slynk:create-server :port 50006 :dont-close t))
-;;   (load init-file :if-does-not-exist :create)
-;;   (sento-user::start-actors)
-;;   (star.consumers::start-rabbit-document-thread :host *rabbit-address* :port *rabbit-port*)
-;;   (star.consumers::start-rabbit-targets-thread :host *rabbit-address* :port *rabbit-port*)
-;;   (init-db)
-;;   (star.frontends.http-api::start-http-api)
-;;   (sento-user::start-target-loader))
-
-
-;; (defun server/command ()
-;;   "A command to greet someone"
-;;   (clingon:make-command
-;;    :name "start"
-;;    :description "start the server"
-;;    :version "0.1.0"
-;;    :authors '("nsaspy <nsaspy@airmail.cc>")
-;;    :license "GPL v3"
-;;    :options (server/options)
-;;    :handler #'server/handler))
-
-(defun reload ()
-  (ql:quickload :starintel-gserver))
-
-(defun start-debugger ()
-  (ql:quickload '("slynk" "bordeaux-threads"))
-  (format t "Creating slynk server on port: ~a" star:*slynk-port*)
-  (slynk:create-server :port star:*slynk-port*))
-
-
-
-(defun main ()
-
-  (let ((init-file "init.lisp"))
+(defun server/handler (cmd)
+  (let ((debugger (clingon:getopt cmd :debugger))
+        (init-file (clingon:getopt cmd :init-value)))
+    (when debugger
+      (slynk:create-server :port 50006 :dont-close t))
+    (load init-file :if-does-not-exist :create)
     (log:info (format nil "Creating ~a worker threads" (serapeum:count-cpus)))
     (setf lparallel:*kernel* (lparallel:make-kernel (serapeum:count-cpus)))
     (load init-file :if-does-not-exist :create)
@@ -73,5 +40,34 @@
 
         if (not (equal thread (bt:current-thread)))
           do (bt:join-thread thread)))
+
+
+
+(defun server/command ()
+  "A command to greet someone"
+  (clingon:make-command
+   :name "start"
+   :description "start the server"
+   :version "0.1.0"
+   :authors '("nsaspy <nsaspy@airmail.cc>")
+   :license "GPL v3"
+   :options (server/options)
+   :handler #'server/handler)
+  )
+
+(defun reload ()
+  (ql:quickload :starintel-gserver))
+
+(defun start-debugger ()
+  (ql:quickload '("slynk" "bordeaux-threads"))
+  (format t "Creating slynk server on port: ~a" star:*slynk-port*)
+  (slynk:create-server :port star:*slynk-port*))
+
+
+
+(defun main ()
+  (let ((app (server/command)))
+    (clingon:run app)))
+
 
 ;; Server command handler:1 ends here
