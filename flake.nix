65ce718774e87181642d0c272ef39876d07c7c60
N545PY <nsaspy@fedora.email>
2025-02-12 13:47:38 -0500

    update flake.nix adding qlot
    Update lock

--- flake.nix
+++ flake.nix
@@ -8,6 +8,14 @@
   outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      newerQlot = self: super: pkgs.sbclPackages.qlot-cli.overrideAttrs (finalAttrs: previousAttrs: {
+          src = pkgs.fetchFromGitHub {
+            owner = "fukamachi";
+            repo = "qlot";
+            rev = "refs/tags/1.6.0";
+            hash = "sha256-j9iT25Yz9Z6llCKwwiHlVNKLqwuKvY194LrAzXuljsE=";
+    };
+      });
     in
     {
       devShell.x86_64-linux =
@@ -18,8 +26,8 @@
             openssl
             rabbitmq-c
             libffi
-            sqlite
-            pre-commit
+            sbclPackages.qlot-cli
+
           ];
           shellHook = ''
             export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath([pkgs.lmdb.out])}:${pkgs.lib.makeLibraryPath([pkgs.openssl pkgs.rabbitmq-c pkgs.libffi pkgs.sqlite])}
