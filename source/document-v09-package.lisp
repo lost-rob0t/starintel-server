ebe9efe6d3f9fde621882a21239292bef001801a
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-25 17:57:20 -0400

    Define StarIntel v0.9 server boundary package

---
+++ source/document-v09-package.lisp
@@ -0,0 +1,12 @@
+(uiop:define-package :star.documents.v09
+  (:use :cl)
+  (:export
+   #:document-value
+   #:document-data
+   #:document-dtype
+   #:document-date-added
+   #:document-transient-p
+   #:ensure-v09-document
+   #:v09-document-p
+   #:v09-document-json
+   #:v09-document-error))
