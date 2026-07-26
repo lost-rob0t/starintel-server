c58f2bc81d6dce45ec14a471a42e6117c42c964d
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-26 16:09:11 -0400

    Restore dev example config in stable release

--- example_configs/init.lisp
+++ example_configs/init.lisp
@@ -7,14 +7,8 @@
 ;; You can invoke sylnk like so
 ;; (start-debugger)
 ;; Set log config path to logs
-;; here it defaults to the /config/logs/star-server.log
-;; Just uncomment this if you want to not use docker the docker volumne
-;; TODO Make a docker vol for logs, instead of writing to the config dir SECURITY risk
-;; (log:config :daily "./logs/gserver.log"
-;;             :file2
-;;             :sane)
-(log:config :daily "/config/logs/gserver.log"
+(log:config :daily "logs/gserver.log"
             :file2
             :sane)
 
-
+;;
