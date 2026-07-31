9e3952f6448e26a8658e5162abf92995080e774f
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-07-30 20:38:59 -0400

    Initialize credential store before HTTP startup

--- source/main.lisp
+++ source/main.lisp
@@ -1,6 +1,5 @@
 (in-package :starintel-gserver)
 
-
 (defun server/options ()
   (list
    (clingon:make-option
@@ -12,84 +11,61 @@
     :env-vars '("STAR_SERVER_INIT_FILE")
     :key :init-value)))
 
+(defun initialize-runtime (init-file)
+  (safe-load-init init-file)
+  (log:info "Creating ~a worker threads" star:*ingest-workers*)
+  (setf lparallel:*kernel*
+        (lparallel:make-kernel star:*ingest-workers*))
+  (star.databases.couchdb:init-db)
+  (star.auth:initialize-auth-store)
+  (star.actors:start-actors
+   :rabbit-host *rabbit-address*
+   :rabbit-vhost "/"
+   :rabbit-port *rabbit-port*
+   :rabbit-user *rabbit-user*
+   :rabbit-password *rabbit-password*)
+  (star.frontends.http-api::start-http-api)
+  (star.rabbit:start-consumers)
+  (star.actors:start-event-consumer 2))
 
-(defun server/handler (cmd)
-  (let ((debugger (clingon:getopt cmd :debugger))
-        (init-file (clingon:getopt cmd :init-value)))
-
-    (safe-load-init init-file)
-    
-    (log:info (format nil "Creating ~a worker threads" star:*ingest-workers*))
-    (setf lparallel:*kernel* (lparallel:make-kernel star:*ingest-workers*))
-    (star.databases.couchdb:init-db)
-    (star.actors:start-actors :rabbit-host *rabbit-address*
-                              :rabbit-vhost "/"
-                              :rabbit-port *rabbit-port*
-                              :rabbit-user *rabbit-user*
-                              :rabbit-password *rabbit-password*)
-    (star.frontends.http-api::start-http-api)
-    (star.rabbit:start-consumers)
-    (star.actors:start-event-consumer 2))
-
+(defun server/handler (command)
+  (initialize-runtime (clingon:getopt command :init-value))
   (loop for thread in (bt:all-threads)
-        if (not (equal thread (bt:current-thread)))
+        unless (equal thread (bt:current-thread))
           do (bt:join-thread thread)))
 
-
-
 (defun server/command ()
-  "Start server"
   (clingon:make-command
    :name "start"
-   :description "start the server"
+   :description "Start the server"
    :authors '("nsaspy <nsaspy@airmail.cc>")
    :license "GPL v3"
    :options (server/options)
    :handler #'server/handler))
 
-
 (defun main/commands ()
-  (list
-   (server/command)))
+  (list (server/command)))
 
-(defun main/handler (cmd)
-  "Print usage/exit"
-  (clingon:print-usage-and-exit cmd t))
-
+(defun main/handler (command)
+  (clingon:print-usage-and-exit command t))
 
 (defun main/command ()
-  (clingon:make-command :name "star-server"
-                        :version *star-server-version*
-                        :description "Starintel unified API and document consuming service."
-                        :authors '("nsaspy <nsaspy@airmail.cc>")
-                        :license "GPL v3"
-                        :handler #'main/handler
-                        :sub-commands (main/commands)))
+  (clingon:make-command
+   :name "star-server"
+   :version *star-server-version*
+   :description "StarIntel unified API and document-consuming service."
+   :authors '("nsaspy <nsaspy@airmail.cc>")
+   :license "GPL v3"
+   :handler #'main/handler
+   :sub-commands (main/commands)))
 
 (defun start-debugger ()
   (format t "Creating slynk server on port: ~a" star:*slynk-port*)
   (slynk:create-server :port star:*slynk-port*))
 
-
-
 (defun main ()
-  (let ((app (main/command)))
-    (clingon:run app)))
-
+  (clingon:run (main/command)))
 
 (defun repl/main (init-file)
-  "Load the server from the repl"
-  
-  (safe-load-init init-file)
-  (log:info (format nil "Creating ~a worker threads" star:*ingest-workers*))
-  (setf lparallel:*kernel* (lparallel:make-kernel star:*ingest-workers*))
-  (star.databases.couchdb:init-db)
-  (star.actors:start-actors :rabbit-host *rabbit-address*
-                            :rabbit-vhost "/"
-                            :rabbit-port *rabbit-port*
-                            :rabbit-user *rabbit-user*
-                            :rabbit-password *rabbit-password*)
-  (star.frontends.http-api::start-http-api)
-  (star.rabbit:start-consumers)
-  (star.actors:start-event-consumer 2))
-
+  "Load and start the server from the REPL."
+  (initialize-runtime init-file))
