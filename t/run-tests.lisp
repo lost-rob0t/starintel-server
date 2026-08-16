c19790852a0b383ca331fb957d0aeba1dc64b03b
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-16 11:12:16 -0400

    #32 require target lease service test suite

--- t/run-tests.lisp
+++ t/run-tests.lisp
@@ -11,6 +11,7 @@
     dataset-export-tests
     couchdb-view-request-tests
     lease-store-contract-tests
+    target-lease-service-tests
     http-boundary-tests
     http-auth-tests
     auth-users-tests
