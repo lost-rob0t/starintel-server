6f428549f6a62b2ea75b37f8ee6004e30215151b
N545PY <nsaspy@fedora.email>
2026-09-16 09:10:47 -0400

    kb: preserve shared predicates across focused files

--- .prolog/kb/index.pl
+++ .prolog/kb/index.pl
@@ -1,5 +1,6 @@
 % Durable project knowledge for starintel-server. Load with:
 %   swipl -g "use_module('.prolog/kb/index.pl')"
+:- multifile root_cause/3, invariant/2, method/2.
 :- ensure_loaded('root-causes.pl').
 :- ensure_loaded('tooling.pl').
 :- ensure_loaded('git-remotes.pl').
