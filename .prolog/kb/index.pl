72e784c87673737e7c68d55ba74dc12989dfefa4
N545PY <nsaspy@fedora.email>
2026-09-16 09:10:32 -0400

    kb: record quarantine collision and safe bulk reconciliation

--- .prolog/kb/index.pl
+++ .prolog/kb/index.pl
@@ -1,5 +1,6 @@
 % Durable project knowledge for starintel-server. Load with:
 %   swipl -g "use_module('.prolog/kb/index.pl')"
-:- use_module('root-causes.pl').
-:- use_module('tooling.pl').
-:- use_module('git-remotes.pl').
+:- ensure_loaded('root-causes.pl').
+:- ensure_loaded('tooling.pl').
+:- ensure_loaded('git-remotes.pl').
+:- ensure_loaded('ulid-quarantine.pl').
